a682d8f00cfef9209c170a8a2bd82e56ef99c3d4
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2025-02-13 16:09:20 +0530

    ignore commit

--- CWFA/Pages/Users/Edit.cshtml.cs
+++ CWFA/Pages/Users/Edit.cshtml.cs
@@ -40,7 +40,6 @@ namespace CWFA.Pages.Users
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
